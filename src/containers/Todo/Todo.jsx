fee74d0178e6eddf92e5f8e9d2deb3baa43121a5
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-10-12 13:58:55 +0100

    create, read, update add to the project

--- src/containers/Todo/Todo.jsx
+++ src/containers/Todo/Todo.jsx
@@ -1,15 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { db } from "../../firebase";
-import { addDoc, collection } from "@firebase/firestore";
+import { addDoc, collection, getDocs, updateDoc, doc } from "@firebase/firestore";
 
 const Todo = () => {
-  
-  addDoc(collection(db, "users"), {
-    firstName: "Alessandro",
-    lastName: "thedev",
+  const [todos, setTodos] = useState([]);
+  // addDoc(collection(db, "todos"), {
+  //   title: "anything",
+  //   description: "anything",
+  //   isCompleted: false,
+  // });
+
+  // UPDATE THE FUNCTION TO ADD TO "TODO" COLLECTION
+  // CHANGE THE OBJECT
+  // TO HAVE TITLE, DESCRIPTION AND IScOMPLETED KEYS
+  // TITLE -> ANY STRING
+  // DESCRIPTION -> ANY STRING
+  // BOOLEAN -> FALSE
+
+  const getTodos = () => {
+    getDocs(collection(db, "todos")).then(docs => {
+      let userTodos = [];
+      // console.log(docs);
+      docs.forEach(doc => {
+        console.log(doc.id, doc.data());
+        const userTodo = { id: doc.id, ...doc.data() };
+        userTodos.push(userTodo);
+      });
+      setTodos(userTodos);
+    });
+  };
+  // CRUD - INTERACT WITH DB
+  // c -> create
+  const createTodo = event => {
+    event.preventDefault();
+    console.log(event);
+
+    const todoTitle = event.target[0].value;
+    const todoDescription = event.target[1].value;
+
+    addDoc(collection(db, "todos"), {
+      title: todoTitle,
+      description: todoDescription,
+      isCompleted: false,
+    });
+
+    getTodos();
+  };
+
+  // R -> READ
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  //   import { doc, updateDoc } from "firebase/firestore";
+
+  // const washingtonRef = doc(db, "cities", "DC");
+
+  // // Set the "capital" field of the city 'DC'
+  // await updateDoc(washingtonRef, {
+  //   capital: true
+  // });
+
+  // U -> UPDATE
+
+  const updateTodo = event => {
+    console.log(event);
+    const docID = event.target.id;
+    const todoRef = doc(db, "todos", docID);
+    updateDoc(todoRef, {
+      isCompleted: true,
+    });
+    getTodos();
+  };
+
+  const todoJSX = todos.map(todo => {
+    const { id, title, description, isCompleted } = todo;
+    return (
+      <div className={isCompleted && "completed"} key={id}>
+        <p>{title}</p>
+        <p>{description}</p>
+        <button id={id} onClick={updateTodo}>
+          Completed
+        </button>
+      </div>
+    );
   });
 
-  return <p>Todo works</p>;
+  return (
+    <section>
+      <h2>Add Todo...</h2>
+      <form onSubmit={createTodo}>
+        <label name="title">Title</label>
+        <input name="title" type="text" />
+        <label name="description">Description</label>
+        <textarea name="description" cols="30" rows="5"></textarea>
+        <button>Submit</button>
+      </form>
+      <h2>Todo...</h2>
+      {todoJSX}
+    </section>
+  );
 };
 
 export default Todo;
--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,13 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // add to this
+  apiKey: "AIzaSyBJ9WgKCWyZGE-ragBILiLK2t6fL4GY9IM",
+  authDomain: "fir-project-a23a9.firebaseapp.com",
+  projectId: "fir-project-a23a9",
+  storageBucket: "fir-project-a23a9.appspot.com",
+  messagingSenderId: "635700915837",
+  appId: "1:635700915837:web:46541785aabd977405cb14",
+  measurementId: "G-3C9L5VKDYM",
 };
 
 // Initialize Firebase
