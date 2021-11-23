e5c828904ae6118ea8d89d98dfe353197312e437
Charlie Richardson <55165280+Charlie-robin@users.noreply.github.com>
2021-11-23 10:00:28 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJ9WgKCWyZGE-ragBILiLK2t6fL4GY9IM",
-  authDomain: "fir-project-a23a9.firebaseapp.com",
-  projectId: "fir-project-a23a9",
-  storageBucket: "fir-project-a23a9.appspot.com",
-  messagingSenderId: "635700915837",
-  appId: "1:635700915837:web:46541785aabd977405cb14",
-  measurementId: "G-3C9L5VKDYM",
+  // YOU NEED YOUR OWN CONFIG
 };
 
 // Initialize Firebase
